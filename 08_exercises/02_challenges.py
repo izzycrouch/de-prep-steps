8bf58093bbb979194f23af664fee220effef6b5a
izzycrouch <belle.crouch00@gmail.com>
2025-09-17 18:20:25 +0100

    Update 02_challenges.py

--- 08_exercises/02_challenges.py
+++ 08_exercises/02_challenges.py
@@ -32,7 +32,19 @@ is_valid_mobile_number('0712345678!') # returns False
 
 
 def is_valid_mobile_number(mobile_number):
-    pass
+    if not mobile_number[1:].isdigit():
+        return False
+    else:
+        if mobile_number.startswith('07') and len(mobile_number) == 11:
+            return True
+        elif mobile_number.startswith('+447') and len(mobile_number) == 13:
+            return True
+        elif mobile_number.startswith('00447') and len(mobile_number) == 14:
+            return True
+        else:
+            return False
+
+
 
 
 @run_test
@@ -156,52 +168,59 @@ sum_digits_from_string('northcoders') # returns 0
 
 
 def sum_digits_from_string(string):
+    sum_of_digits = 0
+    list_of_string = [character for character in string]
+    for value in list_of_string:
+        if value.isdigit():
+            sum_of_digits += int(value)
+    return sum_of_digits
+    
     pass
 
 
-@skip_test
+@run_test
 def sum_digits_from_string_should_return_0_for_empty_string():
     result = sum_digits_from_string("")
     expected = 0
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def sum_digits_from_string_should_return_0_for_non_numeric_string():
     result = sum_digits_from_string("a")
     expected = 0
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def sum_digits_from_string_should_return_integer_for_single_digit():
     result = sum_digits_from_string("5")
     expected = 5
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def sum_digits_from_string_should_return_sum_for_two_digits():
     result = sum_digits_from_string("16")
     expected = 7
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def sum_digits_from_string_should_return_sum_for_three_digits():
     result = sum_digits_from_string("255")
     expected = 12
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def sum_digits_from_string_should_return_sum_for_mixed_string():
     result = sum_digits_from_string("he12ll3")
     expected = 6
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def sum_digits_from_string_should_return_0_for_no_numbers():
     result = sum_digits_from_string("northcoders")
     expected = 0
@@ -229,45 +248,52 @@ get_williams(['William David', 'Cole Williamson']) # returns []
 
 
 def get_williams(names):
+    list_of_williams = []
+    for name in names:
+        list_split_name = name.split()
+        last_name = list_split_name[1]
+        if last_name == 'Williams':
+            list_of_williams.append(name)
+    return list_of_williams
     pass
 
 
-@skip_test
+@run_test
 def get_williams_should_return_empty_list_for_empty_list():
     result = get_williams([])
     expected = []
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def get_williams_should_return_empty_list_for_single_invalid_item():
     result = get_williams(["Kirsty February"])
     expected = []
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def get_williams_should_return_single_valid_item():
     result = get_williams(["David Williams"])
     expected = ["David Williams"]
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def get_williams_should_return_several_valid_items():
     result = get_williams(["David Williams", "Sarah Williams"])
     expected = ["David Williams", "Sarah Williams"]
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def get_williams_should_return_mixed_items():
     result = get_williams(["Kirsty February", "Sam Williams"])
     expected = ["Sam Williams"]
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def get_williams_should_return_empty_list_for_mixed_items_with_rogue_williams():
     result = get_williams(["William David", "Cole Williamson"])
     expected = []
@@ -295,31 +321,41 @@ get_factorials([]) # returns []
 
 
 def get_factorials(numbers):
+    factorials = []
+    for number in numbers:
+        if number == 0 or number == 1:
+            factorials.append(1)
+        else:
+            result = 1
+            for i in range(2, number + 1):
+                result *= i    
+            factorials.append(result)
+    return factorials
     pass
 
 
-@skip_test
+@run_test
 def get_factorials_should_return_empty_list_for_empty_list():
     result = get_factorials([])
     expected = []
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def get_factorials_should_return_single_item():
     result = get_factorials([3])
     expected = [6]
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def get_factorials_should_return_multiple_items():
     result = get_factorials([1, 5, 2])
     expected = [1, 120, 2]
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def get_factorials_should_return_factorial_of_1():
     result = get_factorials([1])
     expected = [1]
@@ -342,59 +378,64 @@ largest_number(43) # returns 43
 
 
 def largest_number(number):
+    string_of_number = str(number)
+    list_of_number = [character for character in string_of_number]
+    reversed_sorted_list_of_number = sorted(list_of_number, reverse = True)
+    reversed_sorted_string_of_number = ''.join(reversed_sorted_list_of_number)
+    return int(reversed_sorted_string_of_number)
     pass
 
 
-@skip_test
+@run_test
 def largest_number_should_return_single_digit():
     result = largest_number(3)
     expected = 3
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def largest_number_should_return_double_digits_in_correct_order():
     result = largest_number(43)
     expected = 43
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def largest_number_should_return_double_digits_in_incorrect_order():
     result = largest_number(34)
     expected = 43
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def largest_number_should_return_double_digits_repeated():
     result = largest_number(44)
     expected = 44
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def largest_number_should_return_triple_digits_in_correct_order():
     result = largest_number(321)
     expected = 321
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def largest_number_should_return_triple_digits_in_incorrect_order():
     result = largest_number(213)
     expected = 321
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def largest_number_should_return_triple_digits_with_two_repeated():
     result = largest_number(233)
     expected = 332
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def largest_number_should_return_lots_of_digits():
     result = largest_number(8456329456)
     expected = 9866554432
@@ -421,31 +462,38 @@ generate_matrix(3) # returns [
 
 
 def generate_matrix(number):
+    matrix = []
+    for _ in range(number):
+        row = [None] * number
+        matrix.append(row)
+    return matrix
+
+
     pass
 
 
-@skip_test
+@run_test
 def generate_matrix_should_return_empty_list_for_zero():
     result = generate_matrix(0)
     expected = []
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def generate_matrix_should_return_single_nested_list_for_single_digit():
     result = generate_matrix(1)
     expected = [[None]]
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def generate_matrix_should_return_two_nested_lists_for_two_digits():
     result = generate_matrix(2)
     expected = [[None, None], [None, None]]
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def generate_matrix_should_return_three_nested_lists_for_three_digits():
     result = generate_matrix(3)
     expected = [[None, None, None], [None, None, None], [None, None, None]]
@@ -481,38 +529,45 @@ one is the right way round with less than 3 pieces of fruit
 
 
 def find_wrong_way_fruit(orchard):
+    if len(orchard) <= 2:
+        return 0
+    else:
+        for fruit in orchard:
+            if orchard.count(fruit) == 1:
+                wrong_way_fruit = fruit
+        return orchard.index(wrong_way_fruit)
     pass
 
 
-@skip_test
+@run_test
 def find_wrong_way_fruit_should_return_zero_for_singleton_list():
     result = find_wrong_way_fruit(["apple"])
     expected = 0
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def find_wrong_way_fruit_should_return_zero_for_list_length_two():
     result = find_wrong_way_fruit(["grape", "eparg"])
     expected = 0
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def find_wrong_way_fruit_should_find_last_item_if_reversed():
     result = find_wrong_way_fruit(["apple", "apple", "elppa"])
     expected = 2
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def find_wrong_way_fruit_should_find_first_item_if_reversed():
     result = find_wrong_way_fruit(["elppa", "apple", "apple"])
     expected = 0
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def find_wrong_way_fruit_should_find_intermediate_reversed_item():
     result = find_wrong_way_fruit(["banana", "ananab", "banana", "banana"])
     expected = 1
