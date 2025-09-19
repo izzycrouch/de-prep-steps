6a5988467bb3ee52645309df4cb69eb45ca34846
izzycrouch <belle.crouch00@gmail.com>
2025-09-19 13:38:30 +0100

    Update 01_sum.py

--- 07_documentation_reading/01_sum.py
+++ 07_documentation_reading/01_sum.py
@@ -7,6 +7,10 @@ from test_api.checks import run_test, format_err_msg
 # DO NOT CHANGE CODE ABOVE THIS LINE
 
 def calculate_price_percentage(percentage_changes):
+    price = 100 + sum(percentage_changes)
+    return price
+
+
     """
     This function is used by a shop to add percentage increases to the price of an item.
     The function will take a list of price increases/decreases as an argument and return the final price as a percentage of the total.
